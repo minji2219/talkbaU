3d7b489c1f2093e3239df3c8aecc7906caee7787
minji2219 <200146@jnu.ac.kr>
2024-05-20 00:37:26 +0900

    home 화면 수정 진행 중

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Header from "../../components/Header";
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <div>
+      <Header />
+      <h1 className="font-bold text-2xl">카카오톡 파일 첨부</h1>
+      <div>
+        <img src="/image/speechBubble.png" alt="말풍선" />
+        <span class="material-symbols-outlined">file_present</span>
+        <button>추천 받기</button>
+        <div>카카오톡 대화 파일을 첨부해보세요!</div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
