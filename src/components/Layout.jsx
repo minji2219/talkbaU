01bc471c10159acffdcafd28e5529820e313fd2c
minji2219 <200146@jnu.ac.kr>
2024-05-20 00:36:52 +0900

    레이아웃 수정

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,8 +3,10 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div className=" max-w-[600px] h-[100vh] mx-auto border-2">
-      <Outlet />
+    <div className="max-w-[600px] h-[100vh] mx-auto border-2">
+      <div className="mx-10 my-20 h-[calc(100%-160px)] bg-red-200">
+        <Outlet />
+      </div>
     </div>
   );
 };
