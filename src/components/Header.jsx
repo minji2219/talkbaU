02fa1a8fcdc10e5f86c2195acce3a31d19919072
minji2219 <200146@jnu.ac.kr>
2024-05-20 00:37:03 +0900

    헤더 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <div></div>
-      <img src="/image/logo.png" alt="logo" />
+    <div className="mt-20">
+      {/* 뒤로가기 버튼 */}
+      <img src="/image/logo.png" alt="logo" width={150} />
     </div>
   );
 };
