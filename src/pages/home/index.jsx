854efbe5b7cf8f071604cc20ed0f8fd50e9f30b0
minji2219 <200146@jnu.ac.kr>
2024-05-20 22:51:03 +0900

    로딩중 페이지 구현

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -7,35 +7,35 @@ const Home = () => {
       <Header />
       <h1 className="font-bold text-2xl my-5">카카오톡 파일 첨부</h1>
 
-      <div className="">
-        <div className="flex justify-center items-center relative">
-          <img
-            src="/image/speechBubble.png"
-            alt="말풍선"
-            width={350}
-            className="mx-auto"
-          />
-          <div className="flex flex-col items-center gap-2 absolute">
-            <label
-              for="file"
-              className="material-symbols-outlined block text-8xl cursor-pointer"
-            >
-              file_present
-            </label>
-            <input type="file" id="file" className="hidden" />
-            {/* config파일에 색 지정 */}
-            <button className="px-4 py-2 bg-[#D9D9D9] rounded-md font-extrabold text-lg hover:opacity-80">
-              추천 받기
-            </button>
-            <div className="text-sm">카카오톡 대화 파일을 첨부해보세요!</div>
-          </div>
-        </div>
+      <div className="flex justify-center items-center relative">
         <img
-          src="/image/fileCharacter.png"
-          alt="파일 첨부 캐릭터"
-          className="ml-[50%]"
+          src="/image/speechBubble.png"
+          alt="말풍선"
+          width={350}
+          className="mx-auto"
         />
+        <div className="flex flex-col items-center gap-2 absolute">
+          <label
+            for="file"
+            className="material-symbols-outlined block text-8xl cursor-pointer"
+          >
+            file_present
+          </label>
+          <input type="file" id="file" className="hidden" />
+          {/* config파일에 색 지정 */}
+          <button className="px-4 py-2 bg-[#D9D9D9] rounded-md font-extrabold text-lg hover:opacity-80">
+            추천 받기
+          </button>
+          <div className="text-sm">카카오톡 대화 파일을 첨부해보세요!</div>
+        </div>
       </div>
+
+      <img
+        src="/image/fileCharacter.png"
+        alt="파일 첨부 캐릭터"
+        className="ml-[50%]"
+      />
+
       <div className="flex items-center gap-2 ml-[10%] font-bold hover:cursor-pointer">
         <img src="/image/book.png" alt="첨부방법" />
         <div>파일 첨부 방법</div>
--- src/pages/loading/index.jsx
+++ src/pages/loading/index.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Header from "../../components/Header";
 
 const Loading = () => {
-  return <div>loading</div>;
+  return (
+    <div>
+      <Header />
+      <div className="mt-44 relative">
+        <img
+          src="/image/loadingClock.png"
+          alt="로딩 시계"
+          className="absolute left-8"
+        />
+        <img
+          src="/image/loadingCharacter.png"
+          alt="로딩 요정"
+          className="mx-auto"
+        />
+        <div className="text-center mt-3">결과를 분석중 입니다.</div>
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
